2a556c24778763c7998c0f342dc7dd567b8c29b5
Nicolas Vieira Lima <nicolas.vlima@hotmail.com>
2021-12-17 11:24:25 -0300

    VideoCard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,79 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 
-import Layout from '../components/Layout';
+import Layout from 'src/components/Layout';
+import VideoCard from 'src/components/VideoCard';
 
-export default function Home() {
+function Home({ data }) {
   return (
-    <Layout title="YouTube - Clone">
-      Clone youtube com Next.js + Material-UI
-      <Button variant="outlined" color="secondary">
-        Primary
-      </Button>
+    <Layout title="YouTube">
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item._id} item xl={3} lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title: 'VIDEO TESTE [PARTE #01]',
+      authorId: 1,
+      authorName: 'Nicolas Lima',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/teste1.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      title:
+        'VIDEO TESTE [PARTE #02]',
+      authorId: 1,
+      authorName: 'Nicolas Lima',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/teste2.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      title: 'VIDEO TESTE [PARTE #03]',
+      authorId: 1,
+      authorName: 'Nicolas Lima',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/teste3.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 4,
+      title: 'VIDEO TESTE [PARTE #04]',
+      authorId: 1,
+      authorName: 'Nicolas Lima',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/teste4.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+  ];
+
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    }, // will be passed to the page component as props
+  };
+}
+
+export default Home;
